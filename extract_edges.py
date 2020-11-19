822413677e7a23fe70dc24b028c34032916d20eb
June Aldea <74009323+junealdea@users.noreply.github.com>
2020-11-19 09:37:51 +0100

    Cambios en el código

--- extract_edges.py
+++ extract_edges.py
@@ -1,116 +1,136 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 13 14:12:35 2020
+Created on Tue Nov 17 14:19:31 2020
 
-@author: june.aldea
+@author: junea
 """
 
-import cv2
 import numpy as np
+import cv2
+import matplotlib.pyplot as plot
 
-input_image = "./images/angio3superf.tif"
-
-# read image 
-image = cv2.imread(input_image,0)           #plot.imshow(image, cmap='gray')
-
-#Valor medio pixeles
-def media(image):
-	size = image.shape
+# Import input image
+image = plot.imread("./images/angio3superf.tif")
+plot.subplot(1,2,1)
+plot.axis("off")
+plot.title("Input image")  
+plot.imshow(image)
+i_gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+size=i_gray.shape
+                    
+#Media de los píxeles
+def media(size):
 	sume = 0
-	dim = size[0]*size[1]                    #columna x fila
+	dim = size[0]*size[1]
 	for i in range(size[0]):
 		for j in range(size[1]):
-			sume += image[i,j]
+			sume += i_gray[i,j]
 	return sume/dim
 
-valormedio = media(image)
+media = media(size)
 
-#Datos imagenes de entrada
-precision = 0.7                              #Valor entre 0-1
-gamma = 2
-t1 = 0
-t2 = valormedio+valormedio*0.2*precision        
-mm = 3                                       #Milímetros de la imagen (3 0 6)
-prof = 0                                     #Profundidad de la imagen (0 o 1)
+#Image processing
+# 1. White top hat 
+# Getting the kernel to be used in Top-Hat 
+filterSize =(11,11) 
+"MORPH_RECT / MORPH_CROSS / MORPH_ELLIPSE"
+elem = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, filterSize) 
 
-#Identificación de bordes
-def canny(image,t1,t2,gamma):
-	"""
-	Implementación de un filtro Gaussiano para la reducción del ruido
-    Estiramiento del histograma para el aumento del contraste
-    Detección de bordes
-
-	Entradas:
-		- image: OCT-A image
-		- t1 and t2:  thresholds of canny edge detector
-		- gamma: gamma parameter to canny edge detector
-	"""
-    #Imagen de salida = cv2.GaussianBlur (Imagen de entrada, tamaño del kernel [anchura, altura], desviación estándar 
-	filteredimage = cv2.GaussianBlur(image,(15,15),gamma)
-    #Imagen de salida = cv2.normalize (Imagen de entrada, Imagen de salida, rango intensidad deseado (0,255), cv2.NORM_MINMAX)
-	normalizedimage = cv2.normalize(filteredimage,0,255,cv2.NORM_MINMAX)
-    #Imagen de salida = cv2.Canny (np.uint8(Imagen de entrada), t1, t2)
-	edges = cv2.Canny(np.uint8(normalizedimage),t1,t2)
-
-	return edges
-
-Bordes = canny(image,t1,t2,gamma)
-
-size = image.shape
-kernelsize = max(((size[0]/100)-3),3)
-kernelsize = kernelsize*2
-
-#Operaciones morfológiccas
-
-def morph(op,image, kernelsize):
+def whitetophat(image, elem):
+    """The white_tophat of an image is defined as the image minus its 
+       morphological opening. This operation returns the bright spots 
+       of the image that are smaller than the structuring element"""
     
-	if (op == 'closed'):
-		se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernelsize,kernelsize))
-		imClosed = cv2.morphologyEx(image, cv2.MORPH_CLOSE, se)
-		return imClosed
-	elif (op == 'open'):
-		se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernelsize,kernelsize))
-		imOpen = cv2.morphologyEx(image, cv2.MORPH_OPEN, se)
-		return imOpen
-	elif (op == 'tophat'):
-		se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernelsize,kernelsize))
-		th = cv2.morphologyEx(image, cv2.MORPH_TOPHAT, se)
-		return th
-	elif (op == 'dilate'):
-		se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernelsize,kernelsize))
-		imDil = cv2.dilate((image *1.0).astype(np.float32),se)
-		return imDil
-	elif (op == 'erode'):
-		se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(kernelsize,kernelsize))
-		imEr = cv2.erode((image *1.0).astype(np.float32),se)
-		return imEr
+    image = image.copy()
+    w_tophat = cv2.morphologyEx(image, cv2.MORPH_TOPHAT,elem)
+    return w_tophat
 
-#Extraccíon de bordes
-def edges_extraction(im,prof,mm,precision):
-"
-	size = im.shape
-	if prof:
-		# canny
-		m = media(im)
-		m = m*255
-		edges = canny(im,0,m+m*0.2*precision,2)
-		# closed
-		ss = max(((size[0]/100)-3),3)
-		ss = ss*2
-		imClosed = morph('closed',edges,ss)
+w_tophat = whitetophat(image, elem)
+plot.subplot(1,2,2)
+plot.axis("off")
+plot.title("White top hat") 
+plot.imshow(w_tophat)
+ 
+# 2. Edges identification
+def canny(w_tophat, filterSize, size):
+    "Reducción de ruido mediante filtro Gaussiano"
+    i_gaussian = cv2.GaussianBlur(w_tophat,filterSize,2)
+    "Normalización de la imagen"
+    i_normalized = cv2.normalize (i_gaussian,None,0,255,cv2.NORM_MINMAX)
+    "Detección de bordes"
+    i_edges = cv2.Canny (np.uint8(i_normalized),0, media+media*0.7*0.2)
+    #edges = cv2.Canny(np.uint8(edges),t1,t2)     t2 = m+m*0.2*precision,2 ¿xq es este el máximo?
+    return i_edges
 
-	else:
-		# canny
-		m = media(im)
-		m = m*255
-		if mm == 6:
-			edges = canny(im,0,m+m*0.8*precision,1.9)
-		edges = canny(im,0,m+m*0.3*precision,1.9)
-		# closed
-		ss = (size[0]/100)-3
-		if ss<3:
-			ss =3
-		ss = ss*2
-		imClosed = morph('closed',edges,ss)
-	im = imClosed.copy()
-	return edges, im
+i_edges = canny(w_tophat, filterSize, size)
+plot.figure()
+plot.axis("off")
+plot.title("Canny - Edges identification") 
+plot.imshow(i_edges, cmap='gray')
+
+# 3. Closed / Opened: para eliminar la mayoría de los falsos positivos
+def closed (i_edges, elem):
+    i_closed = cv2.morphologyEx(i_edges,cv2.MORPH_CLOSE,elem)
+    return i_closed
+
+i_closed = closed(i_edges, elem)
+plot.figure()
+plot.subplot(1,2,1)
+plot.axis("off")
+plot.title("Closing") 
+plot.imshow(i_closed, cmap='gray')
+
+# Hacemos el negativo de la imagen 
+i_inverted = cv2.bitwise_not(i_closed)
+
+elem1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(13, 13)) 
+
+def opened (i_inverted, elem):
+    i_opened = cv2.morphologyEx(i_inverted,cv2.MORPH_OPEN,elem1)
+    return i_opened
+
+i_opened = opened(i_inverted, elem)
+
+plot.subplot(1,2,2)
+plot.axis("off")
+plot.title("Opening") 
+plot.imshow(i_opened, cmap='gray')
+
+# 4. Erode: Ajustar con mayor precisión el contorno de la segmentación a los bordes vasculares circundantes (con las operaciones morfológicas previas pierden precisión)
+
+elem1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1,1)) 
+
+def erode (i_opened, elem1):
+    i_eroded = cv2.erode(i_opened, elem1)
+    return i_eroded
+
+i_eroded = erode(i_opened, elem1)
+
+plot.figure()
+plot.axis("off")
+plot.title("Eroded") 
+plot.imshow(i_eroded, cmap='gray')
+
+#4. Modelo de contorno: eliminar falsos negativos
+
+"Threshold"
+ret,thresh = cv2.threshold(i_eroded, 217, 255,0)
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) 
+i_contours = cv2.drawContours(w_tophat, contours, 5, (0, 255, 0), 1)
+
+plot.figure()
+plot.subplot(1,2,1)
+plot.axis("off")
+plot.title("Contour")
+plot.imshow(i_contours)
+plot.subplot(1,2,2)
+plot.axis("off")
+plot.title("Canny")
+plot.imshow(i_edges, cmap='gray')
+
+
+
+
+
+
+
